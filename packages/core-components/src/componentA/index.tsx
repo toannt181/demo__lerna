fd73f41d4fb6da2280d0396ce48ad8738948d457
toannt181 <toannt181@gmail.com>
2021-09-20 14:58:04 +0700

    feat: add hot reload package

--- packages/core-components/src/componentA/index.tsx
+++ packages/core-components/src/componentA/index.tsx
@@ -4,4 +4,4 @@ interface IProps {
   text: string;
 }
 
-export default (props: IProps) => <h1>Hi {props.text}. I am component A</h1>;
+export default (props: IProps) => <h1>Hi {props.text}. I am component A.</h1>;
